68c060389f646ae5a0a0dfc98e260ed0c6335921
Faith546 <147679088+Faith546@users.noreply.github.com>
2025-11-24 21:59:28 -0600

    unnecessary files

--- CompetitionSite/src/routes/pageRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as pageController from '../controllers/pageControllers.js';
-
-const pageRouter = express.Router();
-
-pageRouter.get('/', pageController.getHomePage);
-
-export {pageRouter};
